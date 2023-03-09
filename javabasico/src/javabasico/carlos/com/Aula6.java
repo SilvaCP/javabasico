f3f16b1ed6a8a905a25e118ec795591a2faf9cab
Carlos <79268743+SilvaCP@users.noreply.github.com>
2023-03-08 23:58:10 -0300

    Add projeto de aulas básica de Java
    Aula do Curso em Vídeo Java básico.

---
+++ javabasico/src/javabasico/carlos/com/Aula6.java
@@ -0,0 +1,17 @@
+/**
+ * Tipos primitivos e manipulação de dados.
+ */
+package javabasico.carlos.com;
+
+public class Aula6 {
+	public static void main(String[] args) {
+		String valor = "30";
+		Integer idade = Integer.parseInt(valor);
+		System.out.println(idade);
+		
+		String valor2 = "1.80"; 
+		float altura = Float.parseFloat(valor2);
+		System.out.printf("Altura é %.4f \n",altura);
+	}
+
+}
